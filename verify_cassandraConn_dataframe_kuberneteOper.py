92c79df577b233b41f57059b7be0275d4d2b16ac
leo2105 <lleonv@uni.pe>
2023-05-16 05:07:25 +0000

    dag cassandra conn and dataframe

---
+++ verify_cassandraConn_dataframe_kuberneteOper.py
@@ -0,0 +1,46 @@
+import sys, os, subprocess, logging, shutil, requests, subprocess
+from datetime import datetime, timedelta
+import argparse
+import csv
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
+from airflow import settings
+
+cmd = 'airflow users list'
+result = subprocess.run(cmd.split(' '), stdout=subprocess.PIPE)
+owner = result.stdout.decode('utf-8').split('\n')[2].split('|')[1].strip()
+
+
+default_args = {
+            'owner' : owner,
+            'depends_on_past': False,
+            'retries' : 0,
+        }
+
+dag = DAG(
+    'cassandraConn_dataframe_kubeOper', catchup=False, default_args=default_args, schedule_interval=None, start_date=days_ago(1),)
+
+bash_command = """cd / && mkdir workspace && cd workspace && \\
+                git clone https://gitlab+deploy-token-1950569:125QUNzezM6ddxcjYiE2@gitlab.com/telconetcv/cassandra_rapids_dags.git && \\
+                python /workspace/cassandra_rapids_dags/airflow/utils/verify_cassandra_connection_and_dataframes.py"""
+
+start = DummyOperator(task_id='First task', dag=dag)
+
+passing = KubernetesPodOperator(namespace='default',
+                          startup_timeout_seconds=900,
+                          image="rapidsai/rapidsai-core:23.02-cuda11.5-base-ubuntu20.04-py3.10",
+                          cmds=["/bin/bash", "-c"],
+                          arguments=[bash_command],
+                          labels={"foo": "bar"},
+                          name="passing-test",
+                          task_id="Second task",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+
+passing.set_upstream(start)
